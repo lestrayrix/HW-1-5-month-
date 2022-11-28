fc4e54b742eff5fb29dd599c5c47c96cee98747c
lestrayrix <starkvinlovebts@gmail.com>
2022-11-28 15:29:43 +0300

    hw

---
+++ src/pages/mainPage/MainPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+function MainPage() {
+    const dispatch = useDispatch()
+
+    const newcount = useSelector(state=>state.count)
+
+    return (
+        <>
+            <h1>{newcount}</h1>
+            <button onClick={() => dispatch({type: 'INCREMENT'})}>+</button>
+            <button onClick={() => dispatch({type: 'DECREMENT'})}>-</button>
+        </>
+    )
+}
+
+export default MainPage;
+
+
